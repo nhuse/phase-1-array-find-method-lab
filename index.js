eb0b4be7514ace64651d7eb5f7bf39862772b9b5
nhuse <nathan.huse@yahoo.com>
2021-06-30 17:00:51 -0500

    commit tree 7e2477bd31f52d2c536dd724a0de34d3384d6e13

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record) {
+    let objAW = record.find(element => { return element.result === "W"});
+    if(objAW) {
+        return objAW.year;
+    }
+    else {
+        return undefined;
+    }  
+}
